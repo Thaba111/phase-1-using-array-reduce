ac82554dd99ce6103ada076fd8b250a831b1fd9c
Edith Thaba <writersherry5@gmail.com>
2023-12-28 13:03:58 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Given array of battery batches
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Use reduce() to calculate the totalBatteries
+const totalBatteries = batteryBatches.reduce((accumulator, currentBatch) => accumulator + currentBatch, 0);
+
+// Output the result
+console.log(totalBatteries); // The totalBatteries will be logged to the console
